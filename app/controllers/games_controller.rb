77d7c5130101766e7c0edaea5808ef8a3201830f
Michael Ries <michael.ries@gmail.com>
2017-05-30 11:16:36 -0500

    add routes and games_controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,23 @@
+class GamesController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def create
+    game = Game.create(game_params)
+  end
+  
+  def update
+    game = Game.find(params[:id])
+    game.update(game_params)
+  end
+
+  def index
+    render json: {games: Game.all.as_json(only: [:id,:state])}
+  end
+
+  private
+  
+  def game_params
+    params.require(:game).permit(:id, state: [])
+  end
+  
+end
