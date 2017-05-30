04e968d5e4cd13e47e8bfaf38a30a26a19aa3235
Michael Ries <michael.ries@gmail.com>
2017-05-30 11:33:48 -0500

    hijack save and previous button clicks

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -2,8 +2,16 @@ $(function() {
     $('tbody td').on('click', function() {
         handleClick(this);
     })
+    $('#save').on('click', function(e) {
+        saveGame();
+        e.preventDefault();
+    })
+    $('#previous').on('click', function(e) {
+        showGames();
+        e.preventDefault();
+    })
 })
 
 function handleClick(el) {
-    console.log(el)
+    doTurn();
 }
