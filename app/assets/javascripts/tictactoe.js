9b915bc8ce4c6658936177b04ae67ac675f5a0f3
neener <janinemrosen@gmail.com>
2017-06-04 21:30:47 -0400

    created load game function

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,24 +1,25 @@
 $(function() {
+    loadGame();
     attachListeners();
 })
 
-// const EMPTY = ["", "", "", "", "", "", "", "", ""];
-const WIN_COMBINATIONS = [
+const EMPTY = ["", "", "", "", "", "", "", "", ""];
+// const WIN_COMBINATIONS = [
 
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+//     [0,1,2],
+//     [3,4,5],
+//     [6,7,8],
+//     [0,3,6],
+//     [1,4,7],
+//     [2,5,8],
+//     [0,4,8],
+//     [2,4,6]
 
-  ]
+//   ]
 
-// var currentGame = new Game(null, EMPTY);
-var currentGame = 0
-// var games = {}
+var currentGame = new Game(null, EMPTY);
+
+var games = {}
 var turn = 0
 
 function attachListeners() {
@@ -88,14 +89,20 @@ function showGames() {
     $('div#games').append($list);
 }
 
-function loadGame(el) {
-    var gameId = el.innerText;
-    currentGame = $.grep(games, function(i) {
-        return i.id == gameId;
-    })[0]
+function loadGame() {
+    // function called to take a game and put it onto the board
+    console.log(currentGame.state);
+    console.log($("#game td"))
+    $("#game td").each((index, element) => {
+        //Turn index of 1d array into two indices for 2d array ie: 8 = [2][2], 5 = [1][2]
+        var row = index < 3 ? 0 : index < 6 ? 1 : 2;
+        var col = index % 3; 
+        element.innerHTML = currentGame.state[row][col] || "";
+    })
 }
 
 function displayError() {
     $('#errors').text("I'm sorry, there's been an error. Please try again.");
 }
+
 
